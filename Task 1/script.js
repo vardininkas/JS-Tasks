92309e069e7f10941b8eed0a2592f5a696126a63
vardininkas <106098776+vardininkas@users.noreply.github.com>
2022-10-11 19:29:56 +0300

    1pabaigta

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,22 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const element = document.getElementById("submit-btn");
+element.addEventListener("click", konvertuok);
+document
+  .getElementById("submit-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+
+function konvertuok() {
+  const ivestiKilogramai = document.getElementById("search").value;
+  document.getElementById("atsakymas").style.visibility = "visible";
+
+  document.getElementById("svaraiReiksme").innerHTML =
+    ivestiKilogramai * 2.2046;
+  document.getElementById("gramaiReiksme").innerHTML = ivestiKilogramai / 0.001;
+  document.getElementById("uncijosReiksme").innerHTML =
+    ivestiKilogramai * 35.274;
+}
